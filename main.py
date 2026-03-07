ceacab2dbdce11c6315b592dfc44f8e54a770c61
Kaledaii <mishant480@gmail.com>
2026-03-08 00:45:23 +0545

    two way

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import ttkbootstrap as ttk
 
 def convert(event=None):
     try:
-        miles=float(number.get())
-        km=miles*1.609
-        result.set(f"{km:.2f} km")
+        value=float(number.get())
+        unit = unit_choice.get()
+        if unit == "Miles → Km":
+            result.set(f"{value * 1.609:.2f} km")
+        elif unit == "Km → Miles":
+            result.set(f"{value / 1.609:.2f} miles")
     except ValueError:
         result.set("Invalid input")
 def only_numbers(P):
@@ -16,12 +19,12 @@ def only_numbers(P):
 
 window=ttk.Window(themename='superhero')
 window.resizable(True,True)
-window.title("Miles to Kilometers Converter")
+window.title("Unit Converter")
 
 WIDTH=500
 HEIGHT=250
 window.geometry(f"{WIDTH}x{HEIGHT}")
-title_label=ttk.Label(window,text="Miles to Kilometers Converter",font="Arial 20 bold italic",bootstyle='primary')
+title_label=ttk.Label(window,text="Unit Converter",font="Arial 20 bold italic",bootstyle='primary')
 title_label.pack()
 
 # input frame
@@ -30,8 +33,13 @@ number=tk.StringVar()
 vcmd=(window.register(only_numbers),"%P")
 entry=ttk.Entry(input_frame,textvariable=number,validate="key",validatecommand=vcmd,bootstyle='success')
 entry.focus()
+
+unit_choice=ttk.Combobox(input_frame,values=['Miles → Km','Km → Miles'],state='readonly',bootstyle='info')
+unit_choice.current(0)
+
 button=ttk.Button(input_frame,text="Convert",cursor='hand2',command=convert,bootstyle='warning-outline')
-entry.pack(side='left',padx=10,fill='x',expand=True)
+entry.pack(side='left',padx=2,fill='x',expand=True)
+unit_choice.pack(side='left',padx=10)
 button.pack(side='left')
 input_frame.pack(pady=20,fill='x',expand=True)
 
